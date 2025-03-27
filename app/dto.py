5d9f5e9a5dd08ba141260969d88c3abd39aefec1
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-03-27 16:10:22 +0530

    dto updated

--- app/dto.py
+++ app/dto.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, Field, validator
 class RecordDTO(BaseModel):
     userId: str = None
     recordId: str = Field(default_factory=lambda: str(uuid.uuid4()))
-    file_reference: str = None 
+    recordName: str = None 
 
-    @validator('file_reference')
+    @validator('recordId')
     def file_reference_must_be_valid(cls, v):
         if v and not isinstance(v, str):
-            raise ValueError("File reference must be a string")
+            raise ValueError("record Id must be a string")
         return v
