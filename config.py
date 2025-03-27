8886049c69c6ece37386ead97fcbe199c9f8202f
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-03-27 16:06:52 +0530

    update mongo uri

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://akila:admin123@cluster0.onqcq.mongodb.net/de-id?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://admin:admin1234@cluster0.bd4gchk.mongodb.net/de-id?retryWrites=true&w=majority&appName=Cluster0")
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
